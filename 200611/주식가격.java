869e00b9da63c8c1cea888962df55cbff5764a5b
sungmen <spe0678@gmail.com>
2020-06-04 15:15:24 +0900

    upload 주식가격

---
+++ 200611/주식가격.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    public int[] solution(int[] prices) 
+    {
+        int[] answer = new int[prices.length];
+            
+        for(int i = 0; i < prices.length; i++) 
+        {
+            for(int j = i + 1; j < prices.length; j++) 
+            {
+                if(prices[j] >= prices[i]) 
+                {
+                    answer[i]++;
+                } 
+                else 
+                {
+                    answer[i]++;
+                    
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+}
