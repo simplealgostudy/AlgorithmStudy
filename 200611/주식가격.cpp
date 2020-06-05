e247707a835d9d98efc0a8753294d21eea3150be
sungmen <spe0678@gmail.com>
2020-06-06 05:54:39 +0900

    edit 주식가격

--- 200611/주식가격.cpp
+++ 200611/주식가격.cpp
@@ -10,7 +10,8 @@ vector<int> solution(vector<int> prices)
     {
         for(int j = i + 1; j < prices.size(); j++)
         {
-            if(prices[j] < prices[i]) {
+            if(prices[j] < prices[i]) 
+            {
                 answer[i]++;
                 break;
             }
