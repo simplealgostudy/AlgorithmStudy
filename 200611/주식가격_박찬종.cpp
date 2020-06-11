84ff974d1cacc29996a7ac09f98b33d375e9b36d
LONYSG <48745172+LONYSG@users.noreply.github.com>
2020-06-11 11:46:21 +0900

    Create 주식가격_박찬종.cpp

---
+++ 200611/주식가격_박찬종.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+ 
+    for (int i = 0; i < prices.size()-1; i++) {
+        for (int j = i+1; j < prices.size(); j++) {
+            if (prices[i] > prices[j]) {
+                answer.push_back(j - i);
+                break;
+            }
+            else if (j == prices.size()-1) {
+                answer.push_back(prices.size() - i-1);
+                break;
+            }
+
+            else if (prices[i]<=prices[j])
+                continue;
+            
+        }
+        
+    }
+    answer.push_back(0);
+    return answer;
+}
+
+int main()
+{
+    vector<int> input1(5);
+
+    input1[0] = 1;
+    input1[1] = 2;
+    input1[2] = 3;
+    input1[3] = 2;
+    input1[4] = 3;
+
+    for (int a : solution(input1)) {
+        cout << a << " ";
+    }
+}
