93f86a56acbfbf2fa1ce0a46fbcb2cd66205f35a
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2021-05-09 03:37:58 +0900

    Update 서로_다른_부분_문자열의_개수_유재광.java

--- 210509/서로_다른_부분_문자열의_개수_유재광.java
+++ 210509/서로_다른_부분_문자열의_개수_유재광.java
@@ -9,28 +9,19 @@ import java.io.InputStreamReader;
 
 public class Main
 {
-  
   public static void main(String[] args)throws IOException
   {
-	
     BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언
     String str = bf.readLine(); //String
-    HashMap<String,String> map = new HashMap<>();//HashMap생성
-    
-    map.clear();//초기화
-    
+    HashMap<String,String> map = new HashMap<>();//HashMap생성 
     for(int i = 0 ; i <=str.length() ; i++){
       for(int j = i ; j <= str.length(); j++){
         String temp = str.substring(i,j); //문자열 자르기
-        if(map.containsKey(temp)){
-        continue;
-        }
-        else{
         map.put(temp,temp);
         }
       }
     }
-    System.out.println(map.size()-1);
+    System.out.println(map.size()-1); //자꾸 공백 들어감
   }
 }
 
