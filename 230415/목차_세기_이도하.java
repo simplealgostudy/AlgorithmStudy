fca1794768d21eb622b60d47ecb3dc7a88e31530
LeeDoHa <66478929+LeeYuHwan@users.noreply.github.com>
2023-04-11 11:37:16 +0900

    Create 목차_세기_이도하.java

---
+++ 230415/목차_세기_이도하.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+//참고 : 전성만 코드
+
+class Pair10{
+    int first;
+    int second;
+
+    public Pair10(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+public class Q25956 {
+
+    static Stack<Pair10> stk = new Stack<>();
+    static int[] answerArr;
+
+    static boolean check(int oldVal){
+        int cnt = 0;
+        int d1 = stk.peek().first;
+        while (!stk.empty() && d1 > oldVal){
+            stk.pop();
+            cnt++;
+
+            if(!stk.empty() && stk.peek().first != d1){
+                int d2 = stk.peek().first;
+
+                if(d2 == d1 - 1){
+                    answerArr[stk.peek().second] = cnt;
+                    d1 = stk.peek().first;
+                    cnt = 0;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        answerArr = new int[N];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+
+            if(i == 0 && l != 1) {
+                System.out.println(-1);
+                System.exit(0);
+            }
+
+            if(!stk.empty() && stk.peek().first > l && ! check(l)){
+                System.out.println(-1);
+                System.exit(0);
+            }
+            stk.push(new Pair10(l, i));
+        }
+
+        if(!stk.empty() && !check(0)){
+            System.out.println(-1);
+            System.exit(0);
+        };
+
+        for(int i = 0; i < N; i++){
+            System.out.println(answerArr[i]);
+        }
+
+    }
+}
